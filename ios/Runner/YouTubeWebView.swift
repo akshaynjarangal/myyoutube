ae6e4b1c39ececd52ea2af4f167bafef446311fd
akshaynjarangal <akshayn010@gmail.com>
2024-09-02 12:33:55 +0530

    added ios webview

---
+++ ios/Runner/YouTubeWebView.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeWebView.swift
+//  Runner
+//
+//  Created by Akshay N on 02/09/24.
+//
+
+import SwiftUI
+
+struct YouTubeWebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeWebView()
+}
